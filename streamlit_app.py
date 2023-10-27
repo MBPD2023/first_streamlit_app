d52da650dacc5a83360ca60208f01726dae6e90c
MBPD2023 <148777070+MBPD2023@users.noreply.github.com>
2023-10-27 14:11:22 +0200

    Update streamlit_app.py
    typo again :(

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas as pd
-import request as rq
+import requests as rq
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") #read csv
 my_fruit_list = my_fruit_list.set_index('Fruit') #set new index for selctor
 
