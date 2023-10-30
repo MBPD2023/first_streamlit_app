d5c6b4d389e85b3705c9caf9bc03a54657ebaead
MBPD2023 <148777070+MBPD2023@users.noreply.github.com>
2023-10-30 11:11:31 +0100

    Update streamlit_app.py
    Changed Header and button, close DB connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,16 +45,17 @@ except URLError as e:
   streamlit.error()
 
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 #connect to snowflake via function
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 #add button
-if streamlit.button('Get fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 #don't run anythiong after here while troubleshooing
@@ -70,4 +71,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
